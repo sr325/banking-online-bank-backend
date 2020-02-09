a641e057dc95be0aa89a0526d4f4985376fd352b
sr325 <git@github.com>
2020-02-09 10:31:11 +0000

    first commit

---
+++ src/test/java/com/online/bank/digital/DigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.bank.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
